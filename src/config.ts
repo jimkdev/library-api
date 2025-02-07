88ba8686ded267085f407641c97c17ced5c7ca46
jimkdev <jimkdev150@gmail.com>
2025-02-07 22:56:14 +0200

    Add database settings

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,23 @@
+type DatabaseSettings = {
+  host: string;
+  port: number;
+  database: string;
+  user: string;
+  password: string;
+};
+
 /* Configuration file */
 export default class AppConfig {
   private static instance: AppConfig;
 
   private readonly host: string;
   private readonly port: number;
+  private readonly dbSettings: DatabaseSettings;
 
   private constructor() {
     this.host = process.env.HOST!;
     this.port = Number.parseInt(process.env.PORT!);
+    this.dbSettings = JSON.parse(process.env.DB ?? "{}");
   }
 
   public static getInstance() {
@@ -24,5 +34,9 @@ export default class AppConfig {
 
   public getPort() {
     return this.port;
+  }
+
+  public getDatabaseSettings() {
+    return this.dbSettings;
   }
 }
