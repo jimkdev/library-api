e8d631a4c45c1a9a5d70bcf5300fa6bdbf2d7a84
jimkdev <jimkdev150@gmail.com>
2025-02-07 20:02:35 +0200

    Delete controller classes

--- src/controllers/controller.ts
+++
@@ -1,7 +0,0 @@
-import UserController from "./users.js";
-
-export default class Controller {
-  public static getUserController(): UserController {
-    return UserController.getInstance();
-  }
-}
