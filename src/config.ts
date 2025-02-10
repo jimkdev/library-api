35d457dd73fd79feb03ce195242fb03216ba9a56
jimkdev <jimkdev150@gmail.com>
2025-02-10 22:28:40 +0200

    Add jwt configuration options in config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { StringValue } from "ms";
+
 type DatabaseSettings = {
   host: string;
   port: number;
@@ -13,11 +15,17 @@ export default class AppConfig {
   private readonly host: string;
   private readonly port: number;
   private readonly dbSettings: DatabaseSettings;
+  private readonly jwtExpirationTime: StringValue;
+  private readonly jwtAccessTokenSecret: string;
+  private readonly jwtRefreshTokenSecret: string;
 
   private constructor() {
     this.host = process.env.HOST!;
     this.port = Number.parseInt(process.env.PORT!);
     this.dbSettings = JSON.parse(process.env.DB ?? "{}");
+    this.jwtExpirationTime = "1H";
+    this.jwtAccessTokenSecret = process.env.ACCESS_TOKEN_SECRET!;
+    this.jwtRefreshTokenSecret = process.env.REFRESH_TOKEN_SECRET!;
   }
 
   public static getInstance() {
@@ -38,5 +46,17 @@ export default class AppConfig {
 
   public getDatabaseSettings() {
     return this.dbSettings;
+  }
+
+  public getJwtExpirationTime(): StringValue {
+    return this.jwtExpirationTime;
+  }
+
+  public getJwtAccessTokenSecret(): string {
+    return this.jwtAccessTokenSecret;
+  }
+
+  public getJwtRefreshTokenSecret(): string {
+    return this.jwtRefreshTokenSecret;
   }
 }
