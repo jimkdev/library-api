c4f089ae3301119adb0ecaadad2ad0f3999f0cb7
jimkdev <jimkdev150@gmail.com>
2025-03-07 19:48:00 +0200

    Create authentication middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,44 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import jwt from "jsonwebtoken";
+import AppConfig from "../config.js";
+
+export function isAuthorized(
+  this: FastifyInstance,
+  req: FastifyRequest,
+  rep: FastifyReply,
+  next: () => void,
+) {
+  const authHeader = req.headers.authorization;
+  const config = AppConfig.getInstance();
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return rep
+      .code(401)
+      .type("application/json")
+      .send(
+        JSON.stringify({
+          code: 401,
+          status: "Unauthorized",
+          message: "Unauthorized",
+        }),
+      );
+  }
+
+  try {
+    jwt.verify(token, config.getJwtAccessTokenSecret());
+    next();
+  } catch (error) {
+    rep
+      .code(401)
+      .type("application/json")
+      .send(
+        JSON.stringify({
+          code: 401,
+          status: "Unauthorized",
+          message: "Unauthorized",
+        }),
+      );
+  }
+}
