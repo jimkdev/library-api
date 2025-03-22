9eb60714a6dc0fd34337eac015dd3a6ac1905f22
jimkdev <jimkdev150@gmail.com>
2025-03-22 15:41:42 +0200

    Create endpoint that retrieves books

--- src/api/books/routes.ts
+++ src/api/books/routes.ts
@@ -13,6 +13,11 @@ type Book = {
   quantity: number;
 };
 
+type PaginationParams = {
+  page: string | number;
+  limit: string | number;
+};
+
 export default fp(function (app: FastifyInstance, opts, done: () => void) {
   const baseUrl = "/books";
 
@@ -68,6 +73,80 @@ export default fp(function (app: FastifyInstance, opts, done: () => void) {
               code: 500,
               status: "Internal server error",
               message: "Book could not be stored!",
+            }),
+          );
+      }
+    },
+  });
+
+  app.route({
+    url: `${baseUrl}`,
+    method: "GET",
+    preHandler: [isAuthorized],
+    handler: async function (req: FastifyRequest, rep: FastifyReply) {
+      try {
+        let { page, limit } = req.query as PaginationParams;
+
+        // TODO Separate pagination logic
+
+        page = Number.parseInt(page.toString());
+        limit = Number.parseInt(limit.toString());
+
+        const offset = limit * (page - 1);
+
+        let query = `SELECT COUNT(b.id) AS count FROM books b`;
+
+        let result = (await this.database.query(query)).rows[0];
+
+        const totalPages = limit > 0 ? Math.ceil(result["count"] / limit) : 1;
+
+        if (page > totalPages) {
+          return rep
+            .code(400)
+            .type("application/json")
+            .send(
+              JSON.stringify({
+                code: 400,
+                status: "Bad request",
+                message:
+                  "Current page number cannot be greater than total pages number!",
+              }),
+            );
+        }
+
+        const nextPage = page >= totalPages ? null : page + 1;
+        const prevPage = page <= 1 ? null : page - 1;
+
+        query = `
+          SELECT b.id, b.title, b.author, b.isbn, b.published_at,
+          b.is_available, b.quantity FROM books b
+          OFFSET $1 LIMIT $2;
+        `;
+
+        result = await this.database.query(query, [offset, limit]);
+
+        const response = {
+          data: [...result.rows],
+          pagination: {
+            totalRecords: result.rowCount,
+            currentPage: page,
+            limit: limit,
+            totalPages: totalPages,
+            nextPage: nextPage,
+            prevPage: prevPage,
+          },
+        };
+        rep.code(200).type("application/json").send(JSON.stringify(response));
+      } catch (error) {
+        console.error(error);
+        rep
+          .code(500)
+          .type("application/json")
+          .send(
+            JSON.stringify({
+              code: 500,
+              status: "Internal server error",
+              message: "Could not fetch books!",
             }),
           );
       }
