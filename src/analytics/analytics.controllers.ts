1765491a12c55d0f6723fd7b6b7c6758c3c84ef3
jimkdev <jameskol10@proton.me>
2026-02-16 22:38:51 +0200

    Format query

--- src/analytics/analytics.controllers.ts
+++ src/analytics/analytics.controllers.ts
@@ -22,16 +22,16 @@ export async function getAnalytics(
            SELECT u.id
            FROM users u
            WHERE u.is_active
-          )
-          SELECT
-            COUNT(bl.id):: BIGINT AS total_book_lendings,
-            COUNT(DISTINCT au.id):: BIGINT AS total_active_users,
-            COUNT(DISTINCT ab.id):: BIGINT AS total_available_books
-          FROM book_lendings bl
-          RIGHT JOIN available_books ab
-            ON bl.book_id = ab.id
-          FULL OUTER JOIN active_users au
-            ON bl.user_id = au.id;
+         )
+         SELECT
+           COUNT(bl.id):: BIGINT AS total_book_lendings,
+           COUNT(DISTINCT au.id):: BIGINT AS total_active_users,
+           COUNT(DISTINCT ab.id):: BIGINT AS total_available_books
+         FROM book_lendings bl
+         RIGHT JOIN available_books ab
+           ON bl.book_id = ab.id
+         FULL OUTER JOIN active_users au
+           ON bl.user_id = au.id;
     `)
     ).rows[0] as Analytics;
 
